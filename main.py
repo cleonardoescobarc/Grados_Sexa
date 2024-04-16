872dd4e124bb60846a29cccc557e5ac84c903181
ACER <71706690@continental.edu.pe>
2024-04-15 23:44:23 -0500

    a

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Convercion de grados a radianes")
+print("Asumamos que pi = 3.1416")
+grados = float(input("Ingrese el valor de grados: "))
+
+gradosR = grados * (2*3.1416/180)
+
+print("La cantidad de radianes es: ", gradosR)
+
+gradosS = gradosR * 180 / (3.1416 * 2)
+
+print("La cantidad de grados sexa es: ", gradosS)
